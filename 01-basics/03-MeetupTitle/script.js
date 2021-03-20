9b4e04a80ea561515cf45b485323eb2d5aae8bae
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2021-03-20 04:40:19 +0300

    meetup title

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -1,3 +1,20 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
-// new Vue();
+let response = await fetch('https://course-vue.javascript.ru/api/meetups');
+let text = await response.text();
+const meetupsData = JSON.parse(text);
+console.log('получено: ', meetupsData);
+
+const app = new Vue({
+  el: '#app',
+  data: (app) => ({
+    meetups: meetupsData,
+    title: this.data.meetups(0).title
+  }),
+  methods: {
+  },
+  computed: {},
+  watch: {}
+});
+
+console.log(meetupsData, app.data.meetups);
