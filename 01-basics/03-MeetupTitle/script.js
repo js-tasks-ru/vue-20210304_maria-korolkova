989604f267b267209c1a120e1f6c3d4230a1a95b
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2021-03-21 17:41:20 +0300

    fix solution for Meetup title

--- 01-basics/03-MeetupTitle/script.js
+++ 01-basics/03-MeetupTitle/script.js
@@ -2,37 +2,40 @@ import Vue from './vendor/vue.esm.browser.js';
 
 const app = new Vue({
   el: '#app',
+  
   data: (app) => ({
     meetup: null,
-    number: 3
+    number: 0
   }),
+
   methods: {
     getMeetupData: function () {
+      if (this.number === 0) return;
       const url = 'https://course-vue.javascript.ru/api/meetups/' + this.number;
-      console.log(url);
       fetch(url) 
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
           this.meetup = data;
         });
     }
   },
+
   created() {
-    console.log(this.number, this.meetup);
     this.getMeetupData();
   },
 
   computed: {
     chosenMeetupTitle: function () {
-      //return '112223333';
+      if (this.meetup === null) return;
       return this.meetup.title;
     }
   },
-  watch: {
-    number: this.getMeetupData()
-  }
 
+  watch: {
+    number: function () {
+      this.getMeetupData();
+    }
+  }
 });
